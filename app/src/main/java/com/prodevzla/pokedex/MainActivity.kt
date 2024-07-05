8b43a759042e1b7147c32952dbc58c8046436dba
Renato Ferrante <rgferranteg@gmail.com>
2024-07-05 15:26:20 +1000

    upgraded kotlin version, added navigation compose and serializable dependencies.

--- app/src/main/java/com/prodevzla/pokedex/MainActivity.kt
+++ app/src/main/java/com/prodevzla/pokedex/MainActivity.kt
@@ -11,7 +11,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.prodevzla.pokedex.ui.theme.PokedexTheme
+import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +23,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PokedexTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+                NavHost(navController, startDestination = Home) {
+                    composable<Home> {
+                        Text("HELLO RENZO")
+                    }
+                    composable<Profile> { backStackEntry ->
+                        Text(text = "BYE RENZO")
+                    }
                 }
             }
         }
@@ -45,3 +52,16 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+
+// Define a home destination that doesn't take any arguments
+@kotlinx.serialization.Serializable
+object Home
+
+// Define a profile destination that takes an ID
+@Serializable
+data class Profile(val id: String)
+
+//Add dependencies
+//navigation compose
+//hilt
+//retrofit
