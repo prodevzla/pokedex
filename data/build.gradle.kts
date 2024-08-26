ea41752173490bf02007b1278c4d19095bfc2523
Renato Ferrante <rgferranteg@gmail.com>
2024-08-26 15:12:36 +1000

    made some changes to the data module so room dependencies could be added (changed from kotlin library module to android module). added room dependencies.

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,16 +1,41 @@
 plugins {
-    id("java-library")
-    kotlin("jvm")
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.apollo)
+    alias(libs.plugins.ksp)
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
+android {
+    namespace = "com.prodevzla.pokedex.data2"
+    compileSdk = 34
 
+    defaultConfig {
+        minSdk = 21
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
 
 dependencies {
+
     implementation(project(":domain"))
 
     implementation(libs.apollo.runtime)
@@ -18,6 +43,13 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.javax.inject)
 
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+//    annotationProcessor(libs.androidx.room.compiler)
+    ksp(libs.androidx.room.compiler)
 }
 
 apollo {
