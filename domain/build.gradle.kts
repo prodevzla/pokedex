e0ef9035c03549acbc00941cf79247f84f673c02
Renato Ferrante <rgferranteg@gmail.com>
2024-09-30 15:06:59 +1000

    removed serialization dependency from the domain module.

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
-    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -35,7 +34,6 @@ android {
 
 dependencies {
     implementation(libs.androidx.core.ktx)
-    implementation(libs.kotlinx.serialization.json)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
